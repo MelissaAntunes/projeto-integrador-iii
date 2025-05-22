ed608de2c6225dfe9ae5651bcb708fa35f8ea2f7
Melissa <melissaantunesdk@gmail.com>
2025-05-22 08:23:44 -0300

    Add a markdown editor

--- app/companies/new/page.tsx
+++ app/companies/new/page.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import { TextField, TextArea, Button } from '@radix-ui/themes'
+"use client";
+
+import { TextField, Button, TextArea } from '@radix-ui/themes'
+import "easymde/dist/easymde.min.css"
+import SimpleMDE from "react-simplemde-editor"
 
 const NewCompanyPage = () => {
   return (
     <div className='max-w-xl space-y-3'>
         <TextField.Root placeholder="Title" />
-        <TextArea placeholder="Description" />
+        {/* <TextArea placeholder="Description" />*/}
+        <SimpleMDE placeholder="Description" />
         <Button>Add new company</Button>
 
     </div>
