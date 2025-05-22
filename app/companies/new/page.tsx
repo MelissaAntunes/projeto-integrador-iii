2fb29250a6c5b14502306c22042adc2487509be0
Melissa <melissaantunesdk@gmail.com>
2025-05-22 08:36:27 -0300

    Handle form submission

--- app/companies/new/page.tsx
+++ app/companies/new/page.tsx
@@ -2,17 +2,44 @@
 
 import { TextField, Button, TextArea } from '@radix-ui/themes'
 import "easymde/dist/easymde.min.css"
-import SimpleMDE from "react-simplemde-editor"
+import { useForm, Controller } from "react-hook-form"
+import axios from "axios"
+import { useRouter } from 'next/navigation';
+
+interface companyForm {
+    name: string
+    description: string
+    logo_url: string
+    email: string
+    phone: string
+    website: string
+    address: string
+}
 
 const NewCompanyPage = () => {
-  return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root placeholder="Title" />
-        {/* <TextArea placeholder="Description" />*/}
-        <SimpleMDE placeholder="Description" />
-        <Button>Add new company</Button>
+    const router = useRouter();
+    const {register, control, handleSubmit} = useForm<companyForm>();
 
-    </div>
+  return (
+    <form className='max-w-xl space-y-3' 
+    onSubmit={handleSubmit(async (data) => {
+        await axios.post('/api/companies', data);
+        router.push('/companies');
+    })}>
+        <TextField.Root placeholder="Name" {...register('name')} />
+        <TextField.Root placeholder="Logo URL" {...register('logo_url')} />
+        <TextField.Root placeholder="Email" {...register('email')} />
+        <TextField.Root placeholder="Phone" {...register('phone')} />
+        <TextField.Root placeholder="Website" {...register('website')} />
+        <TextField.Root placeholder="Address" {...register('address')} />
+        <Controller
+            name="description"
+            control={control}
+            render={({ field }) => <TextArea placeholder="Description" {...field}/>}
+        />
+
+        <Button>Add new company</Button>
+    </form>
   )
 }
 
