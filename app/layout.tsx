b999dd60178bad5e7b968143177addbf38f93b9e
Melissa <melissaantunesdk@gmail.com>
2025-05-22 08:17:29 -0300

    Costumize Radix UI Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,26 @@
 import NavBar from "./NavBar"
 import "./globals.css"
-import { Theme } from "@radix-ui/themes"
+import { Theme, ThemePanel } from "@radix-ui/themes"
+import { Inter } from "next/font/google"
 import '@radix-ui/themes/styles.css'
+import './theme-config.css'
 
-
+const inter = Inter({ 
+  subsets: ["latin"],
+  variable: "--font-inter", 
+})
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body>
-        <Theme>
+        <Theme accentColor="jade">
           <NavBar />
           <main className="p-5">{children}</main>
+          <ThemePanel />
         </Theme>
       </body>
     </html>
